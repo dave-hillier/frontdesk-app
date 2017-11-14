ff59825e17631d36aa2dcc1c85a43b1099bf71b4
Dave Hillier <dave@hillier.co>
2017-11-14 22:48:27 +0000

    Removed anchors due to type error

--- src/AccountMenu.tsx
+++ src/AccountMenu.tsx
@@ -6,17 +6,17 @@ import {
 } from 'react-md';
 
 const AccountMenu = () => {
-  const anchors = {
+  // TODO: this needs adding back in after my patch is accepted
+  /*const anchors = {
     x: DropdownMenu.HorizontalAnchors.INNER_RIGHT,
     y: DropdownMenu.VerticalAnchors.BOTTOM,
-  };
+  };*/
   return (
     <DropdownMenu
       id={`avatar-dropdown-menu`}
       menuItems={['Preferences', 'About', { divider: true }, 'Log out']}
       sameWidth={true}
       simplifiedMenu={false}
-      anchor={anchors}
     >
       <AccessibleFakeButton>
         <Avatar suffix="pink">DH</Avatar>
